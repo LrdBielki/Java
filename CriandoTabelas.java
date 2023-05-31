54b38ee89a4c308d7bfe6ccb40a11a579788e2bf
LrdBielki <128479079+LrdBielki@users.noreply.github.com>
2023-05-31 10:24:20 -0300

    Add files via upload

---
+++ CriandoTabelas.java
@@ -0,0 +1,20 @@
+public class CriandoTabelas {
+
+static String driverJDBC="org.postgresql.Driver"; 
+
+public static void main(String[] args) {
+
+try{
+
+System.out.println("Carregando driver JDBC..."); 
+
+Class.forName (driverJDBC);
+
+System.out.println("Driver carregado!");
+
+}
+catch (Exception e){ 
+        System.out.printf("Falha no carregamento! Se", e);
+            }
+    }
+}
